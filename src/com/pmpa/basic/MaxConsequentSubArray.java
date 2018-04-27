7d2d64b63520e9b6c27e40765d2eecf0a9be437c
jay <maninglwj@163.com>
2018-04-27 08:14:17 +0800

    mid - crossing

--- src/com/pmpa/basic/MaxConsequentSubArray.java
+++ src/com/pmpa/basic/MaxConsequentSubArray.java
@@ -1,5 +1,7 @@
 package com.pmpa.basic;
 
+import java.util.Arrays;
+
 /**
  * Created by jay on 2018/4/17.
  *
@@ -27,17 +29,30 @@ public class MaxConsequentSubArray {
      * @return   数组int[]  包含3个元素，第一个元素是最大连续子数组的起始坐标；第二个元素是最大连续子数组的终止坐标；
      *            第三个元素是最大连续子数组的和。
      */
-    public int[] findCrossingMaxSubArray(int[] a , int m, int r, int n){
+    public static int[] findCrossingMaxSubArray(int[] a , int m, int r, int n){
+        //输出结果3个值，第一个是求得子数组的和，第二个值是最大子数组的左下标，第三个值是最大子数组的右下标。
         int[] output = new int[3];
         //记录左右数组下标
         int left,right;
+        int left_final=r,right_final=r;
         //记录左右数组的和
-        int left_sum =
-
-
-
-
-
+        int left_sum_final = Integer.MIN_VALUE,right_sum_final = Integer.MIN_VALUE;
+        int left_sum = 0,right_sum = 0;
+        left = r ;
+        while(left >= m){
+            left_sum = left_sum + a[left];
+            if (left_sum > left_sum_final) {left_sum_final = left_sum;left_final = left;}
+            left--;
+        }
+        right = r+1;
+        while (right<=n){
+            right_sum = right_sum+ a[right];
+            if (right_sum > right_sum_final) {right_sum_final = right_sum;right_final = right;}
+            right++;
+        }
+        output[0] = left_sum_final + right_sum_final;
+        output[1] = left_final;
+        output[2] = right_final;
         return output;
     }
 
@@ -45,5 +60,9 @@ public class MaxConsequentSubArray {
     public static void main(String[] args)
     {
         int[] test = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
+
+        int[] output = MaxConsequentSubArray.findCrossingMaxSubArray(test,2,8,13);
+        System.out.println(Arrays.toString(output));
+
     }
 }
